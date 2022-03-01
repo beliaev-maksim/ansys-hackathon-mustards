489af6772d955146d01601587c3f5fac60b46639
sthoene <stefan.thoene@ansys.com>
2022-03-01 14:31:57 +0100

    from multi to single obstacle

--- mustards-game/obstacle.py
+++ mustards-game/obstacle.py
@@ -8,42 +8,27 @@ SCREEN_HEIGHT = 900
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf =[]
-        self.rect = []
-        self.count = 5
-        self.pos = []
-        for obs in range(0, self.count):
-            self.randomize_pos()
-        for i, pos in enumerate(self.pos):
-            self.surf.append(pygame.Surface((25, 25)))
-            self.surf[i].fill((255, 0, 0))
-            self.rect.append(self.surf[i].get_rect())
-            self.rect[i].move_ip(pos[0],pos[1])
+        self.pos = (0, 0)
+        self.height = 100
+        self.randomize_pos()
+        self.randomize_height()
+        self.surf = pygame.Surface((25, 25))
+        self.surf.fill((255, 0, 0))
+        self.rect = self.surf.get_rect()
+        self.rect.move_ip(self.pos[0], self.pos[1])
 
     def randomize_pos(self):
         x = random.randint(0, SCREEN_WIDTH-25)
         y = random.randint(0, SCREEN_HEIGHT-25)
-        self.pos.append((x, y))
+        self.pos = (x, y)
+
+    def randomize_height(self):
+        self.height = random.randint(100, 1000)
 
     def reset(self):
-        self.count = 5
-        self.pos = []
-        for obs in range(0, self.count):
-            self.randomize_pos()
-        for i, pos in enumerate(self.pos):
-            self.surf.append(pygame.Surface((25, 25)))
-            self.surf[i].fill((255, 0, 0))
-            self.rect.append(self.surf[i].get_rect())
-            self.rect[i].move_ip(pos[0],pos[1])
-
-    def add_obstacles(self, number):
-        start_count = self.count
-        self.count += number
-        for obs in range(0, number):
-            self.randomize_pos()
-        for i, pos in enumerate(self.pos):
-            if i > start_count-1:
-                self.surf.append(pygame.Surface((25, 25)))
-                self.surf[i].fill((255, 0, 0))
-                self.rect.append(self.surf[i].get_rect())
-                self.rect[i].move_ip(pos[0], pos[1])
+        self.randomize_pos()
+        self.randomize_height()
+        self.surf = pygame.Surface((25, 25))
+        self.surf.fill((255, 0, 0))
+        self.rect = self.surf.get_rect()
+        self.rect.move_ip(self.pos[0], self.pos[1])
